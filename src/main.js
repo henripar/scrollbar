f12f04a1321ba6873b1dc76c7ed946360ef42fe7
Serhat Yuna <serhat.yuna@ceng.deu.edu.tr>
2023-03-05 17:09:09 +0300

    Close color picker when clicked outside of it

--- src/main.js
+++ src/main.js
@@ -5,10 +5,25 @@ import './assets/main.css';
 
 import plausible from './plugins/plausible';
 
+const clickOutside = {
+  beforeMount: (el, binding) => {
+    el.clickOutsideEvent = (event) => {
+      if (!(el == event.target || el.contains(event.target))) {
+        binding.value();
+      }
+    };
+    document.addEventListener('click', el.clickOutsideEvent);
+  },
+  unmounted: (el) => {
+    document.removeEventListener('click', el.clickOutsideEvent);
+  },
+};
+
 createApp(App)
   .use(plausible, {
     domain: 'scrollbar.app',
     hashMode: true,
     trackLocalhost: false,
   })
+  .directive('click-outside', clickOutside)
   .mount('#app');
